5fc2bdd5c56fc43187e3161d8b3ed7cfc13810f9
peter-nikodem <peternikodemjr@gmail.com>
2017-03-26 22:50:33 +0200

    Init.

---
+++ src/main/java/net/nikodem/DigitValindatorApplication.java
@@ -0,0 +1,12 @@
+package net.nikodem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitValindatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitValindatorApplication.class, args);
+	}
+}
